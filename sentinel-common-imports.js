efc8e382a2929835327283766a9117dce59702f9
Sean <sean.meininger@pm.me>
2022-04-18 12:13:33 -0700

    Add list of common functions to look for

---
+++ sentinel-common-imports.js
@@ -0,0 +1,11 @@
+//The policies referred to live here at the time of this writing:
+//https://github.com/hashicorp/terraform-sentinel-policies
+const commonFunctions = [
+  "tfconfig-functions",
+  "tfplan-functions",
+  "tfrun-functions",
+  "tfstate-functions",
+  "aws-functions",
+  "azure-functions",
+  "gcp-functions",
+];
