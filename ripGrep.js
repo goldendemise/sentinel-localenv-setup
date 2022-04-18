a7fbefc5e87d20fd783a119ee0dcbecd82672f6c
Sean <sean.meininger@pm.me>
2022-04-18 09:01:48 -0700

    this might've been a bad idea

--- ripGrep.js
+++ ripGrep.js
@@ -1,2 +1,18 @@
 const { rgPath } = require("@vscode/ripgrep");
 const { spawn } = require("child_process");
+const { once } = require("events");
+
+const ripGrep = async (phrase) => {
+  try {
+    const rg = spawn(rgPath, [phrase, process.cwd()]);
+    let exitCode = undefined;
+    rg.on("exit", (code) => {
+      exitCode = code;
+    });
+  } catch (err) {
+    console.log(`Unexpected error spawning ripgrep search:\n${err}`);
+  }
+
+  //exit code for 'no match' should be 1
+  //https://github.com/BurntSushi/ripgrep/issues/948
+};
